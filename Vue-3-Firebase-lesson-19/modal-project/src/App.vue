3c16e418510e7bee840aa0c59ca2e3443d4594c6
phreakkie <nicolainille@hotmail.com>
2022-06-16 11:27:54 +0200

    update 16-06-22

---
+++ Vue-3-Firebase-lesson-19/modal-project/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+	<h1>{{ title }}</h1>
+	<p>Welcome...</p>
+	<!-- <input type="text" ref="name" />
+	<button @click="handleClick">Click me</button> -->
+	<teleport to=".modals" v-if="showModal">
+		<Modal theme="sale" @close="toggleModal">
+			<template v-slot:links>
+				<a href="">Sign up now</a>
+				<a href="">More info</a>
+			</template>
+			<h1>Ninja Giveaway</h1>
+			<p>Grab your ninja swag for half price!</p>
+		</Modal>
+	</teleport>
+	<teleport to=".modals" v-if="showModalTwo">
+		<Modal @close="toggleModalTwo">
+			<h1>This is a challenge</h1>
+			<p>Challenges are fun</p>
+			<template v-slot:second>
+				<a href="">Click here to learn more about challenges</a>
+			</template>
+		</Modal>
+	</teleport>
+	<button @click.alt="toggleModal">Open Modal (alt)</button>
+	<button @click="toggleModalTwo">Open second Modal)</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+	name: "App",
+	components: { Modal },
+	data() {
+		return {
+			title: "My First Vue App :)",
+
+			showModal: false,
+			showModalTwo: false,
+		};
+	},
+	methods: {
+		handleClick() {
+			console.log(this.$refs.name);
+			this.$refs.name.classList.add("acitve");
+			this.$refs.name.focus();
+		},
+		toggleModal() {
+			this.showModal = !this.showModal;
+		},
+		toggleModalTwo() {
+			this.showModalTwo = !this.showModalTwo;
+		},
+	},
+};
+</script>
+
+<style>
+#app,
+.modals {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+h1 {
+	border-bottom: 1px solid #ddd;
+	display: inline-block;
+	padding-bottom: 10px;
+}
+</style>
